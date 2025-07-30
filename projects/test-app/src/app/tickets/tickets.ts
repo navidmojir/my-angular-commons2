8d6bb94d98b308f9429a0bf2b465cd4d7ca6a2be
navidmojir <154021774+navidmojir@users.noreply.github.com>
2025-07-31 00:36:16 +0330

    .

--- projects/test-app/src/app/tickets/tickets.ts
+++ projects/test-app/src/app/tickets/tickets.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
-import { MyGridComponent, CrudParams, FieldConfig, CustomAction} from 'my-angular-commons2';
+import { MyGridComponent, CrudParams, FieldConfig, CustomAction, ConfirmationDialogComponent} from 'my-angular-commons2';
+
+@Component({
+  selector: 'ticket-search-filters-dialog',
+  templateUrl: 'ticket-search-filters-dialog.html',
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TicketSearchFiltersDialog {}
+
 
 @Component({
   selector: 'app-tickets',
-  imports: [MyGridComponent],
+  imports: [MyGridComponent, MatFormFieldModule, MatInputModule, ReactiveFormsModule,
+    MatButtonModule
+  ],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css'
 })
 export class Tickets {
-gridParams: CrudParams = new CrudParams();
+  gridParams: CrudParams = new CrudParams();
 
-  constructor(private router: Router) {
+  @ViewChild(MyGridComponent) grid!: MyGridComponent;
 
+  constructor(private router: Router,
+    private dialog: Dialog
+  ) {
   }
+
+  filters = new UntypedFormGroup({
+    text: new UntypedFormControl()
+  });
 
   ngOnInit() {
     this.gridParams.baseUrl = "http://localhost:8081";
@@ -35,6 +59,11 @@ gridParams: CrudParams = new CrudParams();
     createAction.onClick = () => this.router.navigate(["ticket-details"]);
     this.gridParams.customGeneralActions.push(createAction);
 
+    // let searchAction = new CustomAction();
+    // searchAction.title = "جستجو";
+    // searchAction.onClick = () => this.openSearchDialog();
+    // this.gridParams.customGeneralActions.push(searchAction);
+
     let showDetailsAction = new CustomAction();
     showDetailsAction.title = 'جزئیات';
     showDetailsAction.onClick = (ticket: any) => this.router.navigate(['ticket-details', ticket.id]);
@@ -42,5 +71,12 @@ gridParams: CrudParams = new CrudParams();
     
   }
 
+  applyFilter() {
+    this.grid.reloadFromPageZero();
+  }
+
+  // openSearchDialog() {
+    // this.dialog.open(TicketSearchFiltersDialog);
+  // }
 
 }
